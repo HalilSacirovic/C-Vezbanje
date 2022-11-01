b39a98e459b535dd9f3b41b986b118e4c3eb1639
HalilSacirovic <halilsacirovic03@gmail.com>
2022-11-01 21:31:42 +0100

    Zadaci

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-main()
+int main()
 {
     // ZADATAK BROJ 3.1
  /*
@@ -77,16 +77,82 @@ if (c>max)
     max=c;
 
 printf("Najveci broj je:%d",max);
+
+
+
+ // ZADATAK 3.7
+
+
+ int a;
+
+ printf("Unesite broj poena ostvarenom na ispitu:");
+ scanf("%d",&a);
+
+ if (a>=101) {
+    printf("Broj bodova prelazi 100");
+ }
+ else if (a>91){
+    printf("Ocena je 10");
+ }
+ else if (a>81){
+ printf("Ocena je 9");
+ }
+
+ else if (a>71){
+ printf("Ocena je 8");
+ }
+
+ else if (a>61){
+ printf("Ocena je 7");
+ }
+
+ else if (a>51){
+ printf("Ocena je 6");
+ }
+ else {
+    printf("Ocena je 5");
+ }
+
+
+
+ // ZADATAK 3.8
+
+ int a,b,x;
+
+ printf("Unesite donju i gornju granicu opsega:");
+ scanf("%d%d",&a,&b);
+
+ printf("Unesite ceo broj x = ");
+ scanf("%d",&x);
+
+ if (a<= x*x  && x*x <= b) {
+    printf ("x*x=%d,pripada oblasti [%d,%d]",x*x,a,b);
+ }
+ else{
+    printf ("x*x=%d, ne pripada oblasti [%d,%d]",x*x,a,b);
+ }
+
+*/
+
+// ZADATAK 3.11
+
+
+float x,y;
+
+printf("Unesite x = ");
+scanf("%f",&x);
+
+if (x<=2 && x>-2){
+    y=2*x;
+}
+else if(x<7 && x>=5) {
+    y=3*x-1;
+}
+else {
+    y=1/x;
+}
+printf ("y = %.2f",y);
+
  return 0;
-
- */
-
- double a,b,c,arr;
-
- printf("Unesite tri realna broja:");
- scanf("%lf%lf%lf",&a,&b,&c);
-
-
-
 
 }
