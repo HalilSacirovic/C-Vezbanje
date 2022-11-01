90958d36f51f57ae2828bbfd80d3da787fcf321e
HalilSacirovic <halilsacirovic03@gmail.com>
2022-11-01 19:56:21 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+main()
+{
+    // ZADATAK BROJ 3.1
+ /*
+int broj;
+
+printf("Unesite broj:");
+scanf("%d",&broj);
+
+if (broj % 2 == 0) {
+    printf("Uneti broj je paran");
+}
+else if (broj %3 == 0 ){
+    printf("Uneti broj je neparan");
+}
+
+
+
+// ZADATAK BROJ 3.2
+
+int broj;
+
+printf("Unesite broj");
+scanf("%d",&broj);
+
+if (broj>0){
+    printf("Uneti broj je pozivan");
+}
+else if (broj < 0){
+    printf("Uneti broj je negativan");
+
+}
+
+else {
+    printf("Uneti broj je 0");
+}
+
+
+// ZADATAK 3.3
+
+
+int a,b;
+
+printf("Unesite broj a:");
+scanf("%d",&a);
+
+
+printf("Unesite broj b:");
+scanf("%d",&b);
+
+if (a>b){
+    printf("Broj a je veci od broja b");
+}
+else if (a<b) {
+    printf("Broj a je manji od broja b");
+}
+else {
+ printf("Broj a je jednak broju b");
+
+}
+
+
+// ZADATAK 3.4
+
+
+int a,b,c,max;
+
+printf("Tri cela broja su:");
+scanf("%d%d%d",&a,&b,&c);
+max = a;
+
+if(b>max)
+    max = b;
+if (c>max)
+    max=c;
+
+printf("Najveci broj je:%d",max);
+ return 0;
+
+ */
+
+ double a,b,c,arr;
+
+ printf("Unesite tri realna broja:");
+ scanf("%lf%lf%lf",&a,&b,&c);
+
+
+
+
+}
